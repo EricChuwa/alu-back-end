103800509c96a615265a3af79266c89761401dee
EricChuwa <eric.c.chuwa@gmail.com>
2026-02-23 23:39:22 +0200

    Attempting to appease the PEP8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,27 +3,31 @@
 """ To-do List Progress Reporter
 Fetches data and displays To-list progress for a given ID. """
 
-import sys
 import requests
+import sys
+
 
 API_URL = "https://jsonplaceholder.typicode.com"
+
 
 def get_employee_data(employee_id):
     response = requests.get(f"{API_URL}/users/{employee_id}")
     response.raise_for_status()
     return response.json()
 
+
 def get_todos(employee_id):
     response = requests.get(f"{API_URL}/todos?userId={employee_id}")
     response.raise_for_status()
     return response.json()
+
 
 def show_progress(employee_id):
     try:
         employee = get_employee_data(employee_id)
     except:
         print(f'Error: Employee with the ID {employee_id} was not found.')
-        
+      
     todos = get_todos(employee_id)
     
     if not todos:
@@ -33,7 +37,7 @@ def show_progress(employee_id):
     completed_tasks = [task for task in todos if task['completed']]
     
     num_completed = len(completed_tasks)
-    
+
     print(f"Employee {employee['name']} is done with tasks({num_completed}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task['title']}")
